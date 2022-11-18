815234b8f65f13d0d369742476d7992e0f6644ac
Kstrohfus <houston11@me.com>
2022-11-18 01:33:27 -0700

    Scroll has been fixed added hidden to all photos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
-const dress = document.querySelector(".dresses")
-const hood = document.querySelectorAll(".hood")
+const dressOne = document.querySelector(".dressOne")
+const dressTwo = document.querySelector(".dressTwo")
+const dressThree = document.querySelector(".dressThree")
+const shirtOne  = document.querySelector(".shirtOne")
+const shirtTwo  = document.querySelector(".shirtTwo")
+const shirtThree  = document.querySelector(".shirtThree")
+const hoodOne = document.querySelector(".hoodOne")
+const hoodTwo = document.querySelector(".hoodTwo")
+const hoodThree = document.querySelector(".hoodThree")
 
-dress.addEventListener("click", function () {
-  dress.classList.add("hide");
+dressOne.addEventListener("mouseover", function () {
+  dressOne.classList.add("hide");
 });
 
+shirtOne.addEventListener("mouseover", function () {
+  shirtOne.classList.add("hide");
+});
+
+hoodOne.addEventListener("mouseover", function () {
+  hoodOne.classList.add("hide");
+});
+
+//Second
+dressTwo.addEventListener("mouseover", function () {
+  dressTwo.classList.add("hide");
+});
+
+shirtTwo.addEventListener("mouseover", function () {
+  shirtTwo.classList.add("hide");
+});
+
+hoodTwo.addEventListener("mouseover", function () {
+  hoodTwo.classList.add("hide");
+});
+
+//Third
+
+dressThree.addEventListener("mouseover", function () {
+  dressThree.classList.add("hide");
+});
+
+shirtThree.addEventListener("mouseover", function () {
+  shirtThree.classList.add("hide");
+});
+
+hoodThree.addEventListener("mouseover", function () {
+  hoodThree.classList.add("hide");
+});
